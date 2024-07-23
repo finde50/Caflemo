451558d1ee90ae808e0be0fba11b16129e0949af
finde50 <finde50.dk@gmail.com>
2024-07-23 15:21:13 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (clickableArea) {
         // Add event listener for clickable area
         clickableArea.addEventListener('click', () => {
-            window.location.href = 'https://www.example.com'; // Replace with your desired URL
+            window.location.href = 'https://www.finde50.com'; // Replace with your desired URL
         });
 
         // Function to toggle visibility
